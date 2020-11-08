a739a45b1267eba75039e65196258eee1c66f54c
Matheus Carvalho <matheus10277@gmail.com>
2020-11-08 00:33:44 -0300

    connected to azure blob storage and created routes for saving and getting images

---
+++ API/API/Controllers/UploadController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using API.Models;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Azure.Storage.Blobs.Models;
+using System.Collections.Generic;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private BlobStorage blob = new BlobStorage();
+
+        [HttpPost]
+        public async Task<string> UploadAsync()
+        {
+            var form = Request.Form;
+            var stream = form.Files[0].OpenReadStream();
+            var extension = Path.GetExtension(form.Files[0].FileName);
+            string fileName = $"{Guid.NewGuid()}{extension}";
+            await blob.UploadFile(stream, fileName);
+
+            return "I hope i've had success";
+        }
+
+        [HttpGet]
+        public async Task<List<BlobItem>> GetAllFiles() => await blob.GetAllFiles();
+
+        [HttpGet("{id}")]
+        public string DownloadFile(string name)
+        {
+            return "Hello Friend";
+        }
+    }
+}
---
+++ API/API/Models/BlobStorage.cs
@@ -0,0 +1,37 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class BlobStorage
+    {
+        private string connectionString;
+        private readonly BlobContainerClient containerClient;
+
+        public BlobStorage()
+        {            
+            this.connectionString = Environment.GetEnvironmentVariable("AZURE_STORAGE_CONNECTIONSTRING");
+            this.containerClient = new BlobContainerClient(connectionString, Environment.GetEnvironmentVariable("CONTAINERNAME"));
+        }
+
+        public async Task UploadFile(Stream stream, string fileName)
+        {
+            BlobClient blobClient = containerClient.GetBlobClient(fileName);
+
+            await blobClient.UploadAsync(stream, true);
+        }
+
+        public async Task<List<BlobItem>> GetAllFiles()
+        {
+            List<BlobItem> blobs = new List<BlobItem>();            
+            await foreach (var item in containerClient.GetBlobsAsync())            
+                blobs.Add(item);            
+
+            return blobs;
+        }
+    }
+}
